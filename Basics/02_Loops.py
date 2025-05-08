082400ce1ddf7a2d10db2840aae4a49477ee4513
VishwaPatel1711 <vishwa1711quqq@gmail.com>
2025-05-08 17:41:29 +0530

    Updated file

--- Basics/02_Loops.py
+++ Basics/02_Loops.py
@@ -24,3 +24,27 @@ List=['apple','orange','banana','watermelon']
 for fruit in List:
     print(fruit)
     
+#Q6 print multiplication table of given number
+num=int(input("enter the number:"))
+for i in range(1,11):
+    print(f"{num} x {i} = {num*i}")
+    
+#Q7 Print Square of numbers from 1 to 10
+for i in range(1,11):
+    print(i*i) 
+    
+#Q8 calculate the sum of numbers from 1 to n
+n=int(input("Enter the number:"))
+sum=0
+for i in range(1,n+1):
+    sum += i
+print(f"{sum}")
+        
+#Q9 print reverse number from 10 to 1
+for i in range (10,0,-1):
+    print(i)
+    
+#Q10 print number that divisable by 5 from 1 to 100
+for i in range(1,101):
+    if i%5==0:
+        print(i)
